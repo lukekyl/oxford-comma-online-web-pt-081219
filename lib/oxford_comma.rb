5b98ef48a83fc3d51b3bbf430aa039be366c48fd
Kyle Luke <th.oldschool@gmail.com>
2019-08-19 23:57:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
+  
   new_string = ""
   array.each { |fruit|
-  if !array.last 
-    new_string << "#{fruit}, "
-  else 
+  if array.last 
     new_string << "and #{array.last}."
+  else 
+    new_string << "#{fruit}, "
   end
   }
   p new_string
