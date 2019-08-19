a4dc96c43930ed72a0c4d913ab0b39157966c06b
Kyle Luke <th.oldschool@gmail.com>
2019-08-19 23:59:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,8 @@ def oxford_comma(array)
   if counter < array.length
     new_string << "#{fruit}, "
     counter += 1
-  elsif counter == array.length
-    new_string << "and #{array.last}."
+  else 
+    new_string << "and #{fruit}."
   end
   }
   p new_string
