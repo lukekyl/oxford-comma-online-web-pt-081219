49b5576991b910264526904d02f06355a894f339
Kyle Luke <th.oldschool@gmail.com>
2019-08-20 00:11:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  new_string = ""
+  array.each { |fruit|
+  if array.length == 1
+    new_string << "#{fruit}"
+  elsif fruit != array.last && array.length == 2
+    new_string << "#{fruit} "
+  elsif fruit != array.last && array.length > 2
+    new_string << "#{fruit}, "
+  else
+    new_string << "and #{fruit}"
+  end
+  }
+  p new_string
 end
