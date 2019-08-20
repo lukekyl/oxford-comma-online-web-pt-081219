c18fc51e5d1217f69508b29c23d235b8e80b549e
Kyle Luke <th.oldschool@gmail.com>
2019-08-20 00:10:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   new_string = ""
   array.each { |fruit|
   if array.length == 1
-    new_string = array.split
+    new_string << "#{fruit}"
   elsif fruit != array.last && array.length == 2
     new_string << "#{fruit} "
   elsif fruit != array.last && array.length > 2
